84e49400ab8221237a557b202e219363d1b5aac7
dahomu <44974821+dahomu@users.noreply.github.com>
2019-01-07 16:17:24 +0100

    Changes
    Added names for each member.
    Added text for each member.

--- PHP/register.php
+++ PHP/register.php
@@ -1,37 +1,36 @@
 <?php
 //Database Related
-$server = "127.0.0.1";
 //$server = "ptvp.myd.infomaniak.com";
+$server = "localhost";
+$dbname = "coding_club_registration";
+$db_table = "user_data";
+$username = "root";
+$password = "";
 //$user = 'ptvp_dmitriy';
 //$pass = 'nations2018';
-$dataBase = "coding_club_registration";
-$db_table = "user_data";
-
 //Form variables
 $name = $_POST["name_input"];
 $email = $_POST["email_input"];
 $year_level = $_POST["year_level_input"];
 $coding_experience = $_POST["coding_experience"];
+$coding_experience_array = json_encode($coding_experience);
 $coding_level = $_POST["coding_level"];
-
-
-
-if( empty($_POST["name_input"]) ) { 
-    echo "Checkbox was left unchecked."; 
+/*
+INSERT INTO table_name (column1, column2, column3,...)
+VALUES (value1, value2, value3,...)
+*/
+$sql_insert = "INSERT INTO `user_data` (`name`,`email`,`year`,`coding_experience`, `coding_level`) VALUES ('$name', '$email', '$year_level','$coding_experience_array','$coding_level')";
+$conn = new mysqli($server, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-else { 
-    echo "Not empty";
+if (mysqli_query($conn, $sql_insert)) {
+    echo "data inserted into DB<br>"; 
+} 
+else {
+    echo "Error: " . $sql_insert . "<br>" . mysqli_error($conn);
 }
+mysqli_close($conn);
 
-function IsChecked($checkname,$value){
-    if(!empty($_POST[$checkname])){
-        foreach($_POST[$checkname] as $checkVal){
-                if($checkVal == $value){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-?>
+echo gettype($coding_experience_array);
 
