15e4b7cc7afa3851e5ad3b6bc8a6c2bf18f16d87
Gaysha1337 <dimitriy.kruglikov@gmail.com>
2019-02-11 15:45:58 +0100

    Updated files
    See title.

--- Coding Club Site/README.md
+++
@@ -1,2 +0,0 @@
-# Coding-club-website
-The web for Nations Coding club. The club will be run every Monday from 15:30 - 17:00 
--- PHP/submit_project.php
+++ PHP/submit_project.php
@@ -1,9 +1,6 @@
 <?php
 //Database Related
 $servername = "127.0.0.1";
-//$server = "ptvp.myd.infomaniak.com";
-//$user = 'ptvp_dmitriy';
-//$pass = 'nations2018';
 $dbname = "coding_club";
 $db_table = "project_submissions";
 $user_data_db_table = "user_data";
@@ -12,6 +9,9 @@ $password = "";
 
 if(isset($_POST["project_name"])){
     $project_name = $_POST["project_name"];
+}
+if(isset($_POST["user_name"])){
+    $user_name = $_POST["user_name"]; 
 }
 if(isset($_POST["user_email"])){
     // Checks to see if the user has registered into the club, before uploading
@@ -28,16 +28,13 @@ else{
 }
 //project-name	user-email	media-links	description
 $conn = new mysqli($servername, $username, $password, $dbname);
-$sql_insert = "INSERT INTO $db_table (`project_name`,`user_email`,`media_links`,`description`) VALUES ('$project_name','$user_email','$media_links','$description')";
+$sql_insert = "INSERT INTO $db_table (`project_name`,`user_name`,`user_email`,`media_links`,`description`) VALUES ('$project_name','$user_name','$user_email','$media_links','$description')";
 $email_verification = mysqli_query($conn,"select * from $user_data_db_table where email='$user_email'");
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 if (mysqli_num_rows($email_verification) < 1){
-    //header("Location: ../HTML/registered.php?message=Member or Email already registered.");
-    //die("ERROR: email not registered");
     echo "ERROR: email not registered";
 }
 else{
